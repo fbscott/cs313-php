563e32fe72714e646a69b57c6b03e334c684ad8f
scott <scottcurrell@gmail.com>
2020-09-22 13:08:29 -0600

    02 Teach - stretch challenges

--- web/assets/js/team.js
+++ web/assets/js/team.js
@@ -2,27 +2,49 @@
 var TTA = TTA || {};
 
 // DOM elements
-TTA.divOne        = document.getElementById('js-div-01');
-// TTA.divTwo        = document.getElementById('js-div-02');
-// TTA.divThree      = document.getElementById('js-div-03');
-TTA.clickMeButton = document.getElementById('js-click-me-button');
-TTA.colorButton   = document.getElementById('js-set-color');
-TTA.form          = document.colors;
-TTA.bgColor       = TTA.form.bg;
+TTA.clickMeButton    = document.getElementById('js-click-me-button');
+TTA.formOne          = document.colorOne;
+TTA.formTwo          = document.colorTwo;
+TTA.formThree        = document.colorThree;
+TTA.bgColorOne       = TTA.formOne.bg1;
+TTA.bgColorTwo       = TTA.formTwo.bg2;
+TTA.bgColorThree     = TTA.formThree.bg3;
+TTA.colorButtonOne   = document.getElementById('js-set-color-one');
+TTA.colorButtonTwo   = document.getElementById('js-set-color-two');
+TTA.colorButtonThree = document.getElementById('js-set-color-three');
+TTA.divOne           = document.getElementById('js-div-01');
+TTA.divTwo           = document.getElementById('js-div-02');
+TTA.divThree         = document.getElementById('js-div-03');
 
 // event listeners
 TTA.clickMeButton.addEventListener('click', e => {
     TTA.alertOnClick(e);
 });
 
-TTA.colorButton.addEventListener('click', e => {
-    TTA.changeColor(e);
+TTA.colorButtonOne.addEventListener('click', e => {
+    TTA.changeColor(e, TTA.divOne, TTA.bgColorOne);
+});
+
+// stretch 01
+$(TTA.colorButtonTwo).on('click', e => {
+    // keep page from refreshing
+    e.preventDefault();
+    $(TTA.divTwo).css('background-color', '#' + TTA.getColor(TTA.bgColorTwo));
+});
+
+// stretch 02
+$(TTA.colorButtonThree).on('click', e => {
+    // keep page from refreshing
+    e.preventDefault();
+    $(TTA.divThree).animate({
+        backgroundColor: '#' + TTA.getColor(TTA.bgColorThree)
+    }, 'slow');
 });
 
 /******************************************************************************
  * ALERT ON CLICK
  * Alert the user when button is clicked.
- * @param  {Event} e button click
+ * @param  {Event} e - button click
  *****************************************************************************/
 TTA.alertOnClick = e => {
     // keep page from refreshing
@@ -33,22 +55,32 @@ TTA.alertOnClick = e => {
 
 /******************************************************************************
  * GET COLOR
- * @return {String} hex color value
+ * @param  {String} input - user-defined hex value
+ * @return {String} hex   - color value
  *****************************************************************************/
-TTA.getColor = () => TTA.bgColor.value;
+TTA.getColor = input => input.value;
 
 /******************************************************************************
- * SET COLOR
- * @param  {String} color hex color value
+ * STYLE ELEMENT
+ * @param  {DOM}    el    - div to color
+ * @param  {String} color - hex color value
  *****************************************************************************/
-TTA.styleDiv = (color) => {
-    TTA.divOne.style.backgroundColor = '#' + color;
+TTA.styleEl = (el, color) => {
+    el.style.backgroundColor = '#' + color;
 };
 
-TTA.changeColor = e => {
+/******************************************************************************
+ * CHANGE DIV BACKGROUND COLOR
+ * Receives an event, DOM element, and user-defined hex color value (minus the
+ * '#'). Calls TTA.styleEl to apply the appropriate background CSS.
+ * @param  {Event}  e     - button click
+ * @param  {DOM}    el    - DOM element to be styled
+ * @param  {String} input - user-defined hex value
+ *****************************************************************************/
+TTA.changeColor = (e, el, input) => {
     // keep page from refreshing
     e.preventDefault();
-    let _color = TTA.getColor();
+    let _color = TTA.getColor(input);
 
-    TTA.styleDiv(_color);
+    TTA.styleEl(el, _color);
 };
