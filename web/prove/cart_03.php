f871a7e9e43b786f5b0099f50a3760fb4aeaae22
scott <scottcurrell@gmail.com>
2020-10-02 22:23:43 -0600

    php shopping cart test commit

--- web/prove/cart_03.php
+++ web/prove/cart_03.php
@@ -5,36 +5,49 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-   <meta charset="UTF-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <link rel="stylesheet" href="../assets/css/_reset.css">
-   <link rel="stylesheet" href="../assets/css/_base.css">
-   <link rel="stylesheet" href="../assets/css/_grid.css">
-   <link rel="stylesheet" href="../assets/css/prove_03.css">
-   <title>Cart</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../assets/css/_reset.css">
+  <link rel="stylesheet" href="../assets/css/_base.css">
+  <link rel="stylesheet" href="../assets/css/_grid.css">
+  <link rel="stylesheet" href="../assets/css/prove_03.css">
+  <title>Cart 03</title>
 </head>
 <body>
-   <div class="row">
-      <div class="column medium-6">
-         <h1>Prove 03</h1>
-      </div>
-      <div class="columns medium-6">
-         <p><?php echo 'Cart: ' . $_SESSION['count']; ?></p>
 
-         <ul>
-         <?php 
-            foreach ($_SESSION['cart'] as $key => $value) {
-               if ($value) {
-                  echo '<li>' . $key . ' ' . $value . '</li>';
-               }
-            }
-         ?>
-         </ul>
+<div class="row">
+  <div class="columns small-6">
+    <h1>Cart 03</h1>
+  </div>
+  <div class="columns small-6">
+    <p><a href="./prove_03.php">back</a></p>
+  </div>
+</div>
 
-        <!-- <p><?php echo "Cart: {$_SESSION['count']}"; ?></p> -->
-        <!-- <p>Cart: <?php print_r($_SESSION['count']) ?></p> -->
-        <p><a href="./prove_03.php">back</a></p>
-      </div>
-   </div>
+<div class="row">
+  <div class="column">
+    <p><?php echo 'Cart: ' . $_SESSION['count']; ?></p>
+
+    <ul>
+    <?php 
+      for ($row = 0; $row < 6; $row++) {
+        if ($_SESSION['cart'][$row]['qty'] > 0) {
+          echo '<li><div class="row"><div class="columns small-6"><p>' .
+               $_SESSION['cart'][$row]['prod'] .
+               '</p><p>' .
+               $_SESSION['cart'][$row]['desc'] .
+               '</p></div><div class="columns small-6"><p>$' .
+               number_format($_SESSION['cart'][$row]['amt'], 2) .
+               '</p></div></div></li>';
+        }
+      }
+    ?>
+    </ul>
+
+    <!-- <p><?php echo "Cart: {$_SESSION['count']}"; ?></p> -->
+    <!-- <p>Cart: <?php print_r($_SESSION['count']) ?></p> -->
+  </div>
+</div>
+
 </body>
 </html>
--- web/prove/prove_03.php
+++ web/prove/prove_03.php
@@ -15,78 +15,114 @@
 </head>
 <body>
 
-  <?php 
+<?php 
 
-    $_SESSION['cart'] = array(
-      "Product 1" => null,
-      "Product 2" => null,
-      "Product 3" => null,
-      "Product 4" => null,
-      "Product 5" => null,
-      "Product 6" => null
-    );
-    $_SESSION['count'];
+  $_SESSION['cart'] = array(
+    array(
+      'prod' => 'Product 1',
+      'amt'  => 15.99,
+      'qty'  => 0,
+      'desc' => 'Lorem ipsum dolor sit amet, consectetur adipiscing.'
+    ),
+    array(
+      'prod' => 'Product 2',
+      'amt'  => 25.99,
+      'qty'  => 0,
+      'desc' => 'Suspendisse rhoncus, ex quis facilisis pulvinar.'
+    ),
+    array(
+      'prod' => 'Product 3',
+      'amt'  => 35.99,
+      'qty'  => 0,
+      'desc' => 'Nullam quis eleifend ipsum. Nulla facilisi.'
+    ),
+    array(
+      'prod' => 'Product 4',
+      'amt'  => 45.99,
+      'qty'  => 0,
+      'desc' => 'Vestibulum ante ipsum primis in faucibus orci.'
+    ),
+    array(
+      'prod' => 'Product 5',
+      'amt'  => 55.99,
+      'qty'  => 0,
+      'desc' => 'Ut fermentum eros eget sapien dapibus, sit amet.'
+    ),
+    array(
+      'prod' => 'Product 6',
+      'amt'  => 65.99,
+      'qty'  => 0,
+      'desc' => 'Fusce mattis pellentesque nibh et elementum.'
+    )
+  );
 
-    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['qty_01']) && $_POST['qty_01'] > 0) {
-      $_SESSION['count'] += $_POST['qty_01'];
-      $_SESSION['cart']['Product 1'] = $_POST['qty_01'];
-    }
+  if (isset($_SESSION['count'])) {
+    $_SESSION['count'] = $_SESSION['count'];
+  } else {
+    $_SESSION['count'] = 0;
+  }
+  
+  for ($row = 0; $row < 6; $row++) {
+    $_SESSION['count'] += $_SESSION['cart'][$row]['qty'];
+  }
 
-   ?>
+ ?>
 
-   <div class="row">
-      <div class="column medium-6">
-         <h1>Prove 03</h1>
-      </div>
-      <div class="columns medium-6">
-        <p><a href="./cart_03.php">
-          <?php
+ <div class="row">
+    <div class="columns small-6">
+       <h1>Prove 03</h1>
+    </div>
+    <div class="columns small-6">
+      <p><a href="./cart_03.php"><?php echo 'Cart: ' . $_SESSION['count']; ?></a></p>
+    </div>
+ </div>
 
-            echo 'Cart: ' . $_SESSION['count'];
+ <div class="row">
+   <div class="column">
+    <form name="products" action="prove_03.php" method="post">
+      <ul id="products">
 
-            ?>
-          </a></p>
-      </div>
-   </div>
-
-   <div class="row">
-     <div class="column">
-      <form name="products" action="prove_03.php" method="post">
-         <ul id="products">
-
-          <li>
-            <div class="row">
-              <div class="column">
-                <p class="product-title">Product 1</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sollicitudin bibendum felis eu luctus.</p>
-              </div>
+        <?php 
+          for ($row = 0; $row < 6; $row++) {
+            if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['qty_' . $row]) && $_POST['qty_' . $row] > 0) {
+               $_SESSION['cart'][$row]['qty'] = $_POST['qty_' . $row];
+               $_SESSION['count'] += $_SESSION['cart'][$row]['qty'];
+            }
+         ?>
+        <li>
+          <div class="row">
+            <div class="column">
+              <p class="product-title"><?php echo $_SESSION['cart'][$row]['prod'] ?></p>
+              <p><?php echo $_SESSION['cart'][$row]['desc'] ?></p>
             </div>
-            <div class="row">
-              <div class="columns small-3">
-                <p>Quantity:</p>
-              </div>
-              <div class="columns small-3">
-                <select name="qty_01">
-                  <option value="1">1</option>
-                  <option value="2">2</option>
-                  <option value="3">3</option>
-                  <option value="4">4</option>
-                  <option value="5">5</option>
-                  <option value="6">6</option>
-                </select>
-              </div>
-              <div class="columns small-6">
-                <p>x $1.00</p>
-              </div>
+          </div>
+          <div class="row">
+            <div class="columns small-3">
+              <p>Quantity:</p>
             </div>
-            <button name="add_01" type="submit" class="js-cart-button">Add to Cart<span class="accent"><span>+</span></span></button>
-          </li>
+            <div class="columns small-3">
+              <select name="<?php echo 'qty_' . $row; ?>">
+                <?php 
+                  $qties = array(0, 1, 2, 3, 4, 5);
+                  foreach ($qties as $qty) {
+                 ?>
+                <option value="<?php echo $qty; ?>"><?php echo $qty; ?></option>
+                <?php } ?>
+              </select>
+            </div>
+            <div class="columns small-6">
+              <p><?php echo 'x $' .  $_SESSION['cart'][$row]['amt'] ?></p>
+            </div>
+          </div>
+          <button name="<?php echo 'add_' . $row; ?>" type="submit" class="js-cart-button">Add to Cart<span class="accent"><span>+</span></span></button>
+        </li>
+        <?php } ?>
 
-         </ul>
-      </form>
-     </div>
+      </ul>
+    </form>
    </div>
+ </div>
 
-   <!-- <script src="../assets/js/prove_03.js"></script> -->
+ <!-- <script src="../assets/js/prove_03.js"></script> -->
 </body>
 </html>
