5ac0bfc4d1171da996497330517878d31d1e102f
scott <scottcurrell@gmail.com>
2020-10-04 15:13:48 -0600

    working shopping cart

---
+++ web/prove/week_03/products.php
@@ -0,0 +1,42 @@
+<?php 
+
+  $products = array(
+    array(
+      'prod' => 'Bacon Chicken Sandwich',
+      'amt'  => 8.99,
+      'desc' => 'Grilled chicken breast, bacon, cheese, lettuce, tomato, onion.',
+      'img'  => 'burger_01.png'
+    ),
+    array(
+      'prod' => 'Double Bacon Burger',
+      'amt'  => 7.49,
+      'desc' => 'Two all-beef patties, bacon, cheese, lettuce, tomato.',
+      'img'  => 'burger_02.png'
+    ),
+    array(
+      'prod' => 'Crispy Chicken Sandwich',
+      'amt'  => 7.99,
+      'desc' => 'Crispy chicken breast, lettuce, pickle, and special sauce.',
+      'img'  => 'burger_03.png'
+    ),
+    array(
+      'prod' => 'Single Cheeseburger',
+      'amt'  => 5.99,
+      'desc' => 'Single patty, American cheese, lettuce, on a butter bun.',
+      'img'  => 'burger_04.png'
+    ),
+    array(
+      'prod' => 'Double Cheeseburger',
+      'amt'  => 5.99,
+      'desc' => 'Two Angus beef patties, two cheeses, too good!',
+      'img'  => 'burger_05.png'
+    ),
+    array(
+      'prod' => 'Grilled Chicken Sandwich',
+      'amt'  => 6.99,
+      'desc' => 'Grilled chicken breast, lettuce, tomato, special sauce, sesame seed bun.',
+      'img'  => 'burger_06.png'
+    )
+  );
+
+ ?>
