8441a6acf116dffc96ad4a649d05601d65b2a9e8
scott <scottcurrell@gmail.com>
2020-10-17 14:04:45 -0600

    Week 05 Prove - WIP

--- web/prove/week_05/ledger.php
+++ web/prove/week_05/ledger.php
@@ -26,7 +26,7 @@
 <div class="row">
   <div class="column">
     <h2>Hello, <?php echo $_SESSION['user'] ?>!</h2>
-    <p>Below is the mileage tracking info for your <?php echo $_SESSION['vehicle']; ?>.</p>
+    <p>Below is the mileage tracking info for your <?php echo $_SESSION['vehicle'] ?>.</p>
   </div>
 </div>
 
