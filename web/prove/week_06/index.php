e9ae0807e7ea4c91bd6297ede33329a178ef7dd2
scott <scottcurrell@gmail.com>
2020-10-23 20:44:10 -0600

    week 06 update - WIP

--- web/prove/week_06/index.php
+++ web/prove/week_06/index.php
@@ -16,31 +16,48 @@
 </head>
 <body>
 <?php 
-  $user = $_POST['user'];
+   $user = $_POST['user'];
 
-  $_SESSION['user'] = $user;
+   $_SESSION['user'] = $user;
 
-  if (isset($_POST['submitQuery'])) {
-    header('Location: vehicle.php');
-  }
+   if (isset($_POST['submitQuery'])) {
+      header('Location: vehicle.php');
+   }
 
-  if (isset($_POST['submitUpdate'])) {
-    header('Location: vehicle.php');
-  }
+   if (isset($_POST['submitUpdate'])) {
+      $name = $_POST['newUser'];
+
+      $stmt = $db->prepare('INSERT INTO filler(first, last, username) VALUES (:first, :last, :username);');
+      $stmt->bindValue(':first', $first, PDO::PARAM_STR);
+      $stmt->bindValue(':last', $last, PDO::PARAM_STR);
+      $stmt->bindValue(':username', $username, PDO::PARAM_STR);
+      $stmt->execute();
+
+      // header('Location: vehicle.php');
+   }
 
  ?>
 <div class="row">
+   <div class="column">
+      <h1>Mileage Tracker</h1>
+   </div>
+</div>
+<div class="row">
    <div class="large-6 columns">
+      <p>Add a new user.</p>
       <!----------------------------------- FORM ----------------------------------->
       <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
-         <label for="newUser">User</label>
-         <input id="newUser" type="text">
-         <input type="submit" value="Submit" name="submitQuery">
+         <label for="first">First</label>
+         <input id="first" type="text">
+         <label for="last">Last</label>
+         <input id="last" type="text">
+         <label for="username">User Name</label>
+         <input id="username" type="text">
+         <input type="submit" value="Submit" name="submitUpdate">
       </form>
       <!----------------------------------- /FORM ---------------------------------->
    </div><!-- /column -->
    <div class="large-6 columns">
-      <h1>Mileage Tracker</h1>
       <p>Select a user.</p>
       <!----------------------------------- FORM ----------------------------------->
       <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
@@ -58,7 +75,7 @@
                <input type="submit" value="Submit" name="submitQuery">
             </div>
             <div class="columns large-6">
-               <input type="delete" value="Delete" name="submitQuery">
+               <input type="submit" value="Delete" name="deleteUser">
             </div>
          </div>
       </form>
