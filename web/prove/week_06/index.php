5b28602acb139599d2a36331ed4b0feb2ab3b592
scott <scottcurrell@gmail.com>
2020-10-23 20:32:32 -0600

    week 06 update - WIP

--- web/prove/week_06/index.php
+++ web/prove/week_06/index.php
@@ -20,45 +20,50 @@
 
   $_SESSION['user'] = $user;
 
-  if (isset($_POST['submit'])) {
+  if (isset($_POST['submitQuery'])) {
     header('Location: vehicle.php');
   }
+
+  if (isset($_POST['submitUpdate'])) {
+    header('Location: vehicle.php');
+  }
+
  ?>
-<div class="row"><div class="large-6 large-offset-3 column">
-
 <div class="row">
-  <div class="column">
-    <h1>Mileage Tracker</h1>
-    <p>Select a user.</p>
-  </div>
-</div>
-
-<!----------------------------------- FORM ----------------------------------->
-<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
-<div class="row">
-  <div class="column">
-
-    <label for="user">User</label>
-    <select id="user" name="user">
-      <option value="" selected="true" disabled> -- </option>
-      <?php 
-        foreach ($db->query('SELECT * FROM filler') as $row) {
-       ?>
-       <option value="<?php echo $row['first']; ?>"><?php echo $row['first']; ?></option>
-      <?php } ?>
-    </select>
-
-  </div>
-</div>
-
-<div class="row">
-  <div class="column">
-    <input type="submit" value="Submit" name="submit">
-  </div>
-</div>
-</form>
-<!----------------------------------- /FORM ---------------------------------->
-
-</div></div>
+   <div class="large-6 columns">
+      <!----------------------------------- FORM ----------------------------------->
+      <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+         <label for="newUser">User</label>
+         <input id="newUser" type="text">
+         <input type="submit" value="Submit" name="submitQuery">
+      </form>
+      <!----------------------------------- /FORM ---------------------------------->
+   </div><!-- /column -->
+   <div class="large-6 columns">
+      <h1>Mileage Tracker</h1>
+      <p>Select a user.</p>
+      <!----------------------------------- FORM ----------------------------------->
+      <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+         <label for="user">User</label>
+         <select id="user" name="user">
+            <option value="" selected="true" disabled> -- </option>
+            <?php 
+            foreach ($db->query('SELECT * FROM filler') as $row) {
+            ?>
+            <option value="<?php echo $row['first']; ?>"><?php echo $row['first']; ?></option>
+            <?php } ?>
+         </select>
+         <div class="row">
+            <div class="columns large-6">
+               <input type="submit" value="Submit" name="submitQuery">
+            </div>
+            <div class="columns large-6">
+               <input type="delete" value="Delete" name="submitQuery">
+            </div>
+         </div>
+      </form>
+      <!----------------------------------- /FORM ---------------------------------->
+   </div><!-- /column -->
+</div><!-- /row -->
 </body>
 </html>
