068cea2145de18e49546d8c450fc3b9d5be3a397
scott <scottcurrell@gmail.com>
2020-10-31 09:43:21 -0600

    proj 01 - WIP

--- web/prove/project_01/delete.php
+++ web/prove/project_01/delete.php
@@ -1,8 +1,8 @@
 <?php 
     include $_SERVER['DOCUMENT_ROOT'] . '/prove/project_01/_db.php';
 
-    $id = "DELETE FROM ledger WHERE fillup_id = '" . $_GET["fillup_id"] . "'";
-    $fillup_id = "DELETE FROM fillup WHERE id = '" . $_GET["fillup_id"] . "'";
+    $id = 'DELETE FROM ledger WHERE fillup_id = $_GET["fillup_id"]';
+    $fillup_id = 'DELETE FROM fillup WHERE id = $_GET["fillup_id"]';
 
     console_log($_GET['fillup_id']);
     console_log($id);
