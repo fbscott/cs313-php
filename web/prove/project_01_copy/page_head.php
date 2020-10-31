8f6272643af8a903aadf84eaf97980945c8aae6a
scott <scottcurrell@gmail.com>
2020-10-31 17:35:56 -0600

    proj 01 - final

--- web/prove/project_01_copy/page_head.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../../assets/css/_reset.css">
-  <link rel="stylesheet" href="../../assets/css/_base.css">
-  <link rel="stylesheet" href="../../assets/css/_grid.css">
-  <link rel="stylesheet" href="../../assets/css/prove_project.css">
