17ac1909f240e23b9fd9912211fe3b4a770ad390
scott <scottcurrell@gmail.com>
2020-10-31 09:33:21 -0600

    proj 01 - WIP

--- web/prove/project_01/delete.php
+++ web/prove/project_01/delete.php
@@ -1,11 +1,12 @@
 <?php 
     include $_SERVER['DOCUMENT_ROOT'] . '/prove/project_01/_db.php';
 
-    $query = "DELETE FROM ledger WHERE id = '" . $_GET["id"] . "'";
+    $fillup_id = "DELETE FROM ledger WHERE fillup_id = '" . $_GET["fillup_id"] . "'";
+    $fillup_id = "DELETE FROM fillup WHERE id = '" . $_GET["fillup_id"] . "'";
 
     console_log($_GET['id']);
 
-    if($result = pg_query($query)){
+    if($result = pg_query($fillup_id)){
         echo "Data Deleted Successfully.";
     }
     else{
