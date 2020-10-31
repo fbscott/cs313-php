067c84e146e40716c31d4fab441b60100cce2f7e
scott <scottcurrell@gmail.com>
2020-10-31 14:24:22 -0600

    proj 01 - WIP

--- web/prove/project_01/delete.php
+++ web/prove/project_01/delete.php
@@ -1,10 +1,7 @@
 <?php 
     include $_SERVER['DOCUMENT_ROOT'] . '/prove/project_01/_db.php';
 
-    $query = 'DELETE ledger, fillup
-              FROM ledger
-              LEFT JOIN fillup ON fillup.id = ledger.fillup_id
-              WHERE ledger.fillup_id = :fillup_id';
+    $query = 'DELETE FROM ledger WHERE fillup_id = :fillup_id';
 
     $stmt = $db->prepare($query);
     $stmt->execute(array(
