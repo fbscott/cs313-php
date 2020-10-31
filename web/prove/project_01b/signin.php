a35cddae4ea5221d1025ccb131e955adb909601f
scott <scottcurrell@gmail.com>
2020-10-30 19:48:51 -0600

    proj 01 - WIP

---
+++ web/prove/project_01b/signin.php
@@ -0,0 +1,14 @@
+<?php 
+include $_SERVER['DOCUMENT_ROOT'] . '/prove/project_01/_db.php';
+include $_SERVER['DOCUMENT_ROOT'] . '/prove/project_01/_page_head.php';
+?>
+  <title>Sign Up</title>
+</head>
+<body>
+  <div class="row">
+    <div class="column">
+      
+    </div>
+  </div>
+</body>
+</html>
