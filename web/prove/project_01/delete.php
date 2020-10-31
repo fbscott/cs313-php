edf642b1f6288a7eb20a2387a1b3cc2ebe36c789
scott <scottcurrell@gmail.com>
2020-10-31 09:53:24 -0600

    proj 01 - WIP

--- web/prove/project_01/delete.php
+++ web/prove/project_01/delete.php
@@ -1,9 +1,10 @@
 <?php 
     include $_SERVER['DOCUMENT_ROOT'] . '/prove/project_01/_db.php';
-    
-    console_log($_GET['fillup_id']);
 
     $query = 'DELETE FROM ledger WHERE fillup_id = :fillup_id';
+
+    console_log($_GET['fillup_id']);
+    console_log($query);
 
     $stmt = $db->prepare($query);
     $stmt->execute(array(
