956c204e3442b930c2485bfb7a8efb58f063fa02
scott <scottcurrell@gmail.com>
2020-10-31 14:40:43 -0600

    proj 01 - WIP

--- web/prove/project_01/index.php
+++ web/prove/project_01/index.php
@@ -1,33 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-   <meta charset="UTF-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <link rel="stylesheet" href="../../assets/css/_reset.css">
-   <link rel="stylesheet" href="../../assets/css/_base.css">
-   <link rel="stylesheet" href="../../assets/css/_grid.css">
-   <link rel="stylesheet" href="../../assets/css/prove_project.css">
-   <title>Mileage Tracker</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../../assets/css/_reset.css">
+  <link rel="stylesheet" href="../../assets/css/_base.css">
+  <link rel="stylesheet" href="../../assets/css/_grid.css">
+  <link rel="stylesheet" href="../../assets/css/prove_project.css">
+  <title>Mileage Tracker</title>
 </head>
 <body>
 
 <div class="row"><div class="large-6 large-offset-3 columns">
 
-   <div class="row">
-     <div class="column">
-       <h1>Mileage Tracker</h1>
-       <p>View previously recorded records or create a new one.</p>
-     </div>
-   </div>
+  <div class="row">
+    <div class="column">
+      <h1>Gas Fill Up Tracker</h1>
+      <p>Log your gas fill-ups by user and vehicle.</p>
+      <p>View previously logged entries or add a new user.</p>
+    </div>
+  </div>
 
-   <div class="row">
-      <div class="large-6 column">
-         <button onclick="location.href = './filler.php';">View Fill-up Records</button>
-      </div>
-      <div class="large-6 column">
-         <button onclick="location.href = './add_filler.php';">Add New Record</button>
-      </div>
-   </div>
+  <div class="row">
+    <div class="large-6 column">
+      <button onclick="location.href = './filler.php';">View Fill-up Records</button>
+    </div>
+    <div class="large-6 column">
+      <span class="or">OR</span>
+      <button onclick="location.href = './add_filler.php';">Add New User</button>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="column">
+      <img src="../../assets/img/pump.jpg" alt="car at gas pump">
+    </div>
+  </div>
 
 </div></div>
 
