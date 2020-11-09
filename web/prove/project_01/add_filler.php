5ae59229b65dc1c4471464bf163750663bb5b257
scott <scottcurrell@gmail.com>
2020-11-08 19:25:18 -0700

    add PHP validation

--- web/prove/project_01/add_filler.php
+++ web/prove/project_01/add_filler.php
@@ -6,62 +6,49 @@
 </head>
 <body>
 <?php 
-/***************************************************************************
-* ADD NEW RECORD
-**************************************************************************/
+    /***************************************************************************
+    * ADD NEW RECORD
+    **************************************************************************/
 
-if (isset($_POST['submitAddFiller'])) {
-    $username = htmlentities($_POST['username']);
-    $first    = htmlentities($_POST['first']);
-    $last     = htmlentities($_POST['last']);
-    // are all inputs filled in
-    $isFormValid = true;
-    $errorMsg = '';
+    if (isset($_POST['submitAddFiller'])) {
+        $username = htmlentities($_POST['username']);
+        $first    = htmlentities($_POST['first']);
+        $last     = htmlentities($_POST['last']);
+        // are all inputs filled in
+        $isFormValid = true;
+        $errorMsg = '';
 
-    // all of these elems should be filled in
-    $elems = array($username, $first, $last);
+        // all of these elems should be filled in
+        $elems = array($username, $first, $last);
 
-    // iterate through inputs, make sure they're all filled in
-    for ($i = 0; $i < sizeof($elems); $i++) { 
-        if (empty($elems[$i])) {
-            $isFormValid = false;
+        // iterate through inputs, make sure they're all filled in
+        for ($i = 0; $i < sizeof($elems); $i++) { 
+            if (empty($elems[$i])) {
+                $isFormValid = false;
+            }
+        }
+
+        if ($isFormValid) {
+            /* filler table */
+            $stmt = $db->prepare('INSERT INTO filler(username, first, last) VALUES (:username, :first, :last);');
+            $stmt->bindValue(':username', $username, PDO::PARAM_STR);
+            $stmt->bindValue(':first', $first, PDO::PARAM_STR);
+            $stmt->bindValue(':last', $last, PDO::PARAM_STR);
+            $stmt->execute();
+
+            $filler_id = $db->lastInsertId('filler_id_seq');
+
+            $_SESSION['filler_id'] = $filler_id;
+
+            header('Location: add_vehicle.php');
+        } else {
+          $errorMsg = '<div class="row">
+                         <div class="column error-msg">
+                           <p><strong>Error:</strong> Please complete all fields.</p>
+                         </div>
+                       </div>';
         }
     }
-
-    if ($isFormValid) {
-        /* filler table */
-        $stmt = $db->prepare('INSERT INTO filler(username, first, last) VALUES (:username, :first, :last);');
-        $stmt->bindValue(':username', $username, PDO::PARAM_STR);
-        $stmt->bindValue(':first', $first, PDO::PARAM_STR);
-        $stmt->bindValue(':last', $last, PDO::PARAM_STR);
-        $stmt->execute();
-
-        $filler_id = $db->lastInsertId('filler_id_seq');
-
-        $_SESSION['filler_id'] = $filler_id;
-
-        header('Location: add_vehicle.php');
-    } else {
-      $errorMsg = '<div class="row">
-                     <div class="column">
-                       <p class="error-msg"><strong>Error:</strong> Please enter a last name.</p>
-                     </div>
-                   </div>';
-    }
-}
-
-/***************************************************************************
-* DELETE A RECORD
-**************************************************************************/
-/*
-if (isset($_POST['submitDelete'])) {
-    $userData = $_POST['user'];
-
-    $stmt = $db->prepare('DELETE FROM ledger WHERE filler_id = :userData');
-    $stmt->bindValue(':userData', $userData, PDO::PARAM_INT);
-    $stmt->execute();
-}
-*/
 ?>
 <div class="row">
   <div class="large-6 large-offset-3 columns">
